1ac4bf489c12b3897b69f22311a935e4848324e8
Brandon Little <brlittle86@gmail.com>
2017-09-18 10:15:55 -0700

    built out the FPS class

--- OOPPractice/OOPPractice/FPS.cs
+++ OOPPractice/OOPPractice/FPS.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace OOPPractice
 {
-    class FPS
+    class FPS : VideoGame
     {
+        public int Price { get; set; }
+        public bool Online { get; set; }
+
+        public override void Cost(int price)
+        {
+            Console.WriteLine($"This game costs ${price}.");
+            Price = price;
+        }
+
+        public override void IsAwesome(bool confirm)
+        {
+            if (confirm == true)
+            {
+                Console.WriteLine("This game has a storyline.");
+            }
+            else
+            {
+                Console.WriteLine("This game is Call of Duty.");
+            }
+        }
+
+        public void HasCoolWeapons()
+        {
+            Console.WriteLine("This game has cool weapons.");
+        }
     }
 }
