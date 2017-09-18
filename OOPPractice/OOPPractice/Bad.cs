8841bc1791488a80864402607be32475acc6cc3e
Brandon Little <brlittle86@gmail.com>
2017-09-18 10:31:40 -0700

    built out the Bad class

--- OOPPractice/OOPPractice/Bad.cs
+++ OOPPractice/OOPPractice/Bad.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace OOPPractice
 {
-    class Bad
+    class Bad : Music
     {
+        public bool IsCountry { get; set; }
+
+        public override void Cost(int price)
+        {
+            Console.WriteLine($"This garbage song costs ${price}.");
+        }
+
+        public override void HasMusicVideo()
+        {
+            Console.WriteLine("I dunno... probably.");
+        }
+
+        public void ShouldBurn()
+        {
+            Console.WriteLine("Yep. This should definitely burn.");
+        }
     }
 }
