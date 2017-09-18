ec23565776e52dcd9e5c7ea9bb3dd905ec9ebd40
Brandon Little <brlittle86@gmail.com>
2017-09-18 09:58:44 -0700

    created base class files; built Media abstract class

---
+++ OOPPractice/OOPPractice/Media.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPPractice
+{
+    abstract class Media
+    {
+        public string Title { get; set; }
+        public string Release { get; set; }
+
+        public void IsReleased()
+        {
+            Console.WriteLine("This item has been released!");
+        }
+
+        public virtual void IsAvailable()
+        {
+            Console.WriteLine("This item is available for purchase!");
+        }
+
+        public abstract void Cost();
+    }
+}
