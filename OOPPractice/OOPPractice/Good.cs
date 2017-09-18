fec90c43c9ce9be5b7fc9fc81049feadd0d4f52f
Brandon Little <brlittle86@gmail.com>
2017-09-18 10:29:43 -0700

    built out the Good class

--- OOPPractice/OOPPractice/Good.cs
+++ OOPPractice/OOPPractice/Good.cs
@@ -4,7 +4,24 @@ using System.Text;
 
 namespace OOPPractice
 {
-    class Good
+    class Good : Music
     {
+        public string InstrumentType { get; set; }
+
+        public override void Cost(int price)
+        {
+            Console.WriteLine($"This song costs ${price}.");
+            Price = price;
+        }
+
+        public override void HasMusicVideo()
+        {
+            Console.WriteLine("This song has a music video!");
+        }
+
+        public void HasGoodBeat()
+        {
+            Console.WriteLine("You better believe this song will make you move.");
+        }
     }
 }
