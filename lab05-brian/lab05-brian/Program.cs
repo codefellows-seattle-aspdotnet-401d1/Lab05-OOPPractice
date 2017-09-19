72d922a10497af9d037d1a0c68bd2723b69f2efd
Brian McFarlane <bmcfarlane82@gmail.com>
2017-09-18 20:57:54 -0700

    Added most the ship classes. Doing a safety ACP

---
+++ lab05-brian/lab05-brian/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace lab05_brian
+{
+    internal abstract class Ship
+    {
+        internal void Fly()
+        {
+            Console.WriteLine("Zoom zoom!");
+        }
+
+        internal void UseGate()
+        {
+            Console.WriteLine("Changing System");
+        }
+
+        internal void Eject()
+        {
+            Console.WriteLine("Mistakes were made");
+        }
+    }
+
+    internal abstract class Battleship : Ship
+    {
+        internal abstract void HightSlots();
+        internal abstract void MidSlots();
+        internal abstract void LowSlots();
+    }
+
+    internal abstract class AmarrBattleships : Battleship
+    {
+        internal virtual void ResistanceProfile()
+        {
+            int explosive = 85;
+            int 
+        }
+    }
+
+    internal abstract class BlackOps : AmarrBattleships
+    {
+
+    }
+
+    internal class Redeemer : BlackOps
+    {
+
+    }
+
+    internal abstract class Logistics : AmarrBattleships
+    {
+
+    }
+
+    internal abstract class Armageddon
+    {
+        
+    }
+
+    internal abstract class Attack : AmarrBattleships
+    {
+        
+    }
+
+    internal class Abaddon : Attack
+    {
+        
+    }
+
+    internal class Apocalypse : Attack
+    {
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
