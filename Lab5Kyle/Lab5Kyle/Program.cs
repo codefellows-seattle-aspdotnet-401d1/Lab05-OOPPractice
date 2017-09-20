19689c99922bf5b87db2c66d293d23e73888ad04
Kyle Richmond <kylerichmond79@gmail.com>
2017-09-19 19:58:54 -0700

    init commit

---
+++ Lab5Kyle/Lab5Kyle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab5Kyle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
